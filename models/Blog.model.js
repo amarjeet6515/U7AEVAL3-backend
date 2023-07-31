fb740f40f7722bb53f6710a1167220add8990725
Amarjeet Ray <amarjeetray6515@gmail.com>
2023-07-31 22:16:05 +0530

    correction

--- models/Blog.model.js
+++ models/Blog.model.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
     title :{ type: String, required: true },
     category : { type: String, required: true },
-    author : { type: String, required: true },
+    author : { type: String },
     content :{ type: String, required: true },
-    author_id: { type: String, required: true}
+    author_id: { type: String}
 })
 
 const Blogmodel = mongoose.model("blog", blogSchema)
